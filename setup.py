7edd0853958fba0925e143b401f647ac58a4c710
utkugokcen13 <utku.gokcen@hotmail.com>
2022-03-09 13:32:24 +0300

    Initial commit

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     # TODO: Write a globally unique name which will be listed on PyPI
-    name="very-simple-dictionary",
-    author="Emre Sülün",  # TODO: Write your name
+    name="pypi-AgEIcHlwaS5vcmcCJDI1MWVhNGFkLTg2MDctNDM1Zi1hMzIyLWNhZTFiNjllYjJmNgACJXsicGVybWlzc2lvbnMiOiAidXNlciIsICJ2ZXJzaW9uIjogMX0AAAYguvBuVDNP2fByig2w6yWPIFZzvsW90R3KRVrIznIKkTc",
+    author="Utku Gökçen",  # TODO: Write your name
     version="2.0.0",
     packages=["dictionary"],
     install_requires=[
